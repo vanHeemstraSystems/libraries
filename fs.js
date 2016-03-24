d6ff44578a4bef62155821821b15ad84f2ef221a
willem-vanheemstrasystems <willem@vanheemstrasystems.com>
2016-03-24 14:38:59 +0000

    Modified

---
+++ fs.js
@@ -0,0 +1,5 @@
+//import Fs from 'fs';
+//
+//export default Fs;
+
+module.exports = require('fs');
---
+++ uri.js
@@ -0,0 +1,7 @@
+//import URI from 'uri';
+//
+//export default URI;
+
+// see also https://github.com/webr3/URI
+
+module.exports = require('uri');
