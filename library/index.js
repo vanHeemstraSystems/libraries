5a1869e6cdbe547c562056a07d4bcbadf3fe50fa
willem-vanheemstrasystems <willem@vanheemstrasystems.com>
2016-04-24 18:53:11 +0100

    Modified

--- library/index.js
+++ library/index.js
@@ -46,11 +46,307 @@ var LibraryXml2js = require(__dirname+'/xml2js.js');
 function Library() { }
 
 /**
+ * Create a new LibraryAlt object.
+ * @return {LibraryAlt}
+ */
+Library.prototype.alt = function() {
+  return new LibraryAlt();
+}
+
+/**
+ * Create a new LibraryAsync object.
+ * @return {LibraryAsynv}
+ */
+Library.prototype.async = function() {
+  return new LibraryAsync();
+}
+
+/**
+ * Create a new LibraryBabelRegister object.
+ * @return {LibraryBabelRegister}
+ */
+Library.prototype.babelRegister = function() {
+  return new LibraryBabelRegister();
+}
+
+/**
+ * Create a new LibraryBluebird object.
+ * @return {LibraryBluebird}
+ */
+Library.prototype.bluebird = function() {
+  return new LibraryBluebird();
+}
+
+/**
+ * Create a new LibraryBodyParser object.
+ * @return {LibraryBodyParser}
+ */
+Library.prototype.bodyParser = function() {
+  return new LibraryBodyParser();
+}
+
+/**
+ * Create a new LibraryCreateBrowserHistory object.
+ * @return {LibraryCreateBrowserHistory}
+ */
+Library.prototype.createBrowserHistory = function() {
+  return new LibraryCreateBrowserHistory();
+}
+
+/**
+ * Create a new LibraryDevice object.
+ * @return {LibraryDevice}
+ */
+Library.prototype.device = function() {
+  return new LibraryDevice();
+}
+
+/**
+ * Create a new LibraryExpressSession object.
+ * @return {LibraryExpressSession}
+ */
+Library.prototype.expressSession = function() {
+  return new LibraryExpressSession();
+}
+
+/**
+ * Create a new LibraryExpress object.
+ * @return {LibraryExpress}
+ */
+Library.prototype.express = function() {
+  return new LibraryExpress();
+}
+
+/**
+ * Create a new LibraryFs object.
+ * @return {LibraryFs}
+ */
+Library.prototype.fs = function() {
+  return new LibraryFs();
+}
+
+/**
+ * Create a new LibraryHead object.
+ * @return {LibraryHead}
+ */
+Library.prototype.head = function() {
+  return new LibraryHead();
+}
+
+/**
+ * Create a new LibraryHistory object.
+ * @return {LibraryHistory}
+ */
+Library.prototype.history = function() {
+  return new LibraryHistory();
+}
+
+/**
+ * Create a new LibraryHttp object.
+ * @return {LibraryHttp}
+ */
+Library.prototype.http = function() {
+  return new LibraryHttp();
+}
+
+/**
+ * Create a new LibraryI18n object.
+ * @return {LibraryI18n}
+ */
+Library.prototype.i18n = function() {
+  return new LibraryI18n();
+}
+
+/**
+ * Create a new LibraryKoaBody object.
+ * @return {LibraryKoaBody}
+ */
+Library.prototype.koaBody = function() {
+  return new LibraryKoaBody();
+}
+
+/**
+ * Create a new LibraryKoaRouter object.
+ * @return {LibraryKoaRouter}
+ */
+Library.prototype.koaRouter = function() {
+  return new LibraryKoaRouter();
+}
+
+/**
+ * Create a new LibraryKoaValidate object.
+ * @return {LibraryKoaValidate}
+ */
+Library.prototype.koaValidate = function() {
+  return new LibraryKoaValidate();
+}
+
+/**
+ * Create a new LibraryKoa object.
+ * @return {LibraryKoa}
+ */
+Library.prototype.koa = function() {
+  return new LibraryKoa();
+}
+
+/**
+ * Create a new LibraryMocha object.
+ * @return {LibraryMocha}
+ */
+Library.prototype.mocha = function() {
+  return new LibraryMocha();
+}
+
+/**
+ * Create a new LibraryMorgan object.
+ * @return {LibraryMorgan}
+ */
+Library.prototype.morgan = function() {
+  return new LibraryMorgan();
+}
+
+/**
+ * Create a new LibraryPass object.
+ * @return {LibraryPass}
+ */
+Library.prototype.pass = function() {
+  return new LibraryPass();
+}
+
+/**
+ * Create a new LibraryPassportFacebook object.
+ * @return {LibraryPassportFacebook}
+ */
+Library.prototype.passportFacebook = function() {
+  return new LibraryPassportFacebook();
+}
+
+/**
+ * Create a new LibraryPassportLocal object.
+ * @return {LibraryPassportLocal}
+ */
+Library.prototype.passportLocal = function() {
+  return new LibraryPassportLocal();
+}
+
+/**
+ * Create a new LibraryPassport object.
+ * @return {LibraryPassport}
+ */
+Library.prototype.passport = function() {
+  return new LibraryPassport();
+}
+
+/**
  * Create a new LibraryPath object.
  * @return {LibraryPath}
  */
 Library.prototype.path = function() {
   return new LibraryPath();
+}
+
+/**
+ * Create a new LibraryPromise object.
+ * @return {LibraryPromise}
+ */
+Library.prototype.promise = function() {
+  return new LibraryPromise();
+}
+
+/**
+ * Create a new LibraryReactDom object.
+ * @return {LibraryReactDom}
+ */
+Library.prototype.reactDom = function() {
+  return new LibraryReactDom();
+}
+
+/**
+ * Create a new LibraryReactRouter object.
+ * @return {LibraryReactRouter}
+ */
+Library.prototype.reactRouter = function() {
+  return new LibraryReactRouter();
+}
+
+/**
+ * Create a new LibraryReact object.
+ * @return {LibraryReact}
+ */
+Library.prototype.react = function() {
+  return new LibraryReact();
+}
+
+/**
+ * Create a new LibraryRequest object.
+ * @return {LibraryRequest}
+ */
+Library.prototype.request = function() {
+  return new LibraryRequest();
+}
+
+/**
+ * Create a new LibraryRequire object.
+ * @return {LibraryRequire}
+ */
+Library.prototype.require = function() {
+  return new LibraryRequire();
+}
+
+/**
+ * Create a new LibraryRethinkdb object.
+ * @return {LibraryRethinkdb}
+ */
+Library.prototype.rethinkdb = function() {
+  return new LibraryRethinkdb();
+}
+
+/**
+ * Create a new LibraryRethinkdbdash object.
+ * @return {LibraryRethinkdbdash}
+ */
+Library.prototype.rethinkdbdash = function() {
+  return new LibraryRethinkdbdash();
+}
+
+/**
+ * Create a new LibrarySwig object.
+ * @return {LibrarySwig}
+ */
+Library.prototype.swig = function() {
+  return new LibrarySwig();
+}
+
+/**
+ * Create a new LibraryUnderscore object.
+ * @return {LibraryUnderscore}
+ */
+Library.prototype.underscore = function() {
+  return new LibraryUnderscore();
+}
+
+/**
+ * Create a new LibraryUri object.
+ * @return {LibraryUri}
+ */
+Library.prototype.uri = function() {
+  return new LibraryUri();
+}
+
+/**
+ * Create a new LibraryValidator object.
+ * @return {LibraryValidator}
+ */
+Library.prototype.validator = function() {
+  return new LibraryValidator();
+}
+
+/**
+ * Create a new LibraryXml2js object.
+ * @return {LibraryXml2js}
+ */
+Library.prototype.xml2js = function() {
+  return new LibraryXml2js();
 }
 
 //ORIGINAL module.exports = new Library();
