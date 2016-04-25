0edc1830278a364d5381ebf292302f5b29677f58
willem-vanheemstrasystems <willem@vanheemstrasystems.com>
2016-04-25 13:35:48 +0100

    Modified

--- library/path.js
+++ library/path.js
@@ -2,4 +2,6 @@
 //
 //export default Path;
 
-module.exports = require('path');
+// See also https://nodejs.org/api/path.html
+
+module.exports = function() { return(require('path')) }; // Exception, Path module needs to be wrapped in a function
